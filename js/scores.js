802425e117789563439bdc78072f2dd17f7e6d8b
HansEJC <66210680+HansEJC@users.noreply.github.com>
2020-06-08 13:39:15 +0100

    Fixed scores issue

--- js/scores.js
+++ js/scores.js
@@ -1,7 +1,7 @@
 function saveScores(scr) {
 	let scoreID = "jumpScores";
-	scoreID = document.cookie == "game=mole" ? "moleScores" : scoreID; 
-	scoreID = document.cookie == "game=simon" ? "simonScores" : scoreID; 
+	scoreID = document.cookie.includes('game=mole') ? "moleScores" : scoreID; 
+	scoreID = document.cookie.includes('game=simon') ? "simonScores" : scoreID; 
 	let scoreArr = [];
 	const name = document.getElementById("userName");
 	let exists = false;
