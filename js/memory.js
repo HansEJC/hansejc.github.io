823833e00fbf799dc7f040fb195b65da5a074e32
HansEJC <66210680+HansEJC@users.noreply.github.com>
2021-02-08 16:23:15 +0000

    Test

--- js/memory.js
+++ js/memory.js
@@ -129,15 +129,17 @@ function k(w,x,y){
 }
 
 try {
-	const allScore = JSON.parse(localStorage.getItem("simonScores"));	
-	allScore.sort((a,b) => b[1] - a[1]);			
-	const table = document.getElementById('board');
-	allScore.forEach(val => {
-		let row = table.insertRow(-1);
-		row.insertCell(0).innerHTML = val[0];
-		row.insertCell(1).innerHTML = val[1];
-	});
-
+	(async () => {
+		const allScore = await fetch('./uploads/simonscores.json')
+			.then(result => result.json());		
+		allScore.sort((a,b) => b[1] - a[1]);			
+		const table = document.getElementById('board');
+		allScore.forEach(val => {
+			let row = table.insertRow(-1);
+			row.insertCell(0).innerHTML = val[0];
+			row.insertCell(1).innerHTML = val[1];
+		});
+	})();
 }catch(err){}
 
 board.classList.add('waiting');
