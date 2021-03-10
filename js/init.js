d282ebcb6af6b68faf54e37eba48e1a9b6a9e7f8
HansEJC <66210680+HansEJC@users.noreply.github.com>
2021-03-10 17:34:52 +0000

    Revert "Format f3d696c"
    This reverts commit a404b8f6605ce0f97c611d238a7b94c0aab1a358.

--- js/init.js
+++ js/init.js
@@ -2,34 +2,29 @@
 //Toggle Dark Mode
 (function () {
   //Add toggle to page
-  const switchLabel = document.createElement("label");
-  const toggLabel = document.createElement("label");
-  const checkBox = document.createElement("input");
-  const spanny = document.createElement("span");
-  switchLabel.classList.add("switch");
+  const switchLabel = document.createElement('label');
+  const toggLabel = document.createElement('label');
+  const checkBox = document.createElement('input');
+  const spanny = document.createElement('span');
+  switchLabel.classList.add('switch');
   checkBox.id = "DarkToggle";
   checkBox.type = "checkbox";
-  spanny.classList.add("slider", "round");
+  spanny.classList.add('slider', 'round');
   toggLabel.innerText = "Dark Mode";
-  toggLabel.classList.add("toggLabel");
+  toggLabel.classList.add('toggLabel');
   switchLabel.appendChild(checkBox);
   switchLabel.appendChild(spanny);
   document.querySelector("#header").appendChild(switchLabel);
   document.querySelector("#header").appendChild(toggLabel);
 
   // Use matchMedia to check the user preference
-  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
   const darkToggle = document.querySelector("#DarkToggle");
 
   //Toggle to change mode manually
-  darkToggle.addEventListener("change", function () {
-    toggleDarkTheme(darkToggle.checked);
-    saveCheckbox(this);
-  });
+  darkToggle.addEventListener('change', function() {toggleDarkTheme(darkToggle.checked); saveCheckbox(this);});
 
-  toggleDarkTheme(
-    prefersDark.matches && getSavedValue(darkToggle.id) != "false"
-  );
+  toggleDarkTheme(prefersDark.matches && (getSavedValue(darkToggle.id) != "false"));
   if (getSavedValue(darkToggle.id) == "true") toggleDarkTheme(true); //iif statement as it would turn off if false
   //if ((prefersDark.matches || (getSavedValue(darkToggle.id) == "true")) && !darkToggle.checked) darkToggle.click();
 
@@ -38,28 +33,27 @@
 
   // Add or remove the "dark" class based on if the media query matches
   function toggleDarkTheme(shouldAdd) {
-    document.body.classList.toggle("dark", shouldAdd);
+    document.body.classList.toggle('dark', shouldAdd);
     darkToggle.checked = shouldAdd;
   }
+
 })();
 
 function toggleNav(e) {
   const x = document.getElementById("myTopnav");
-  try {
+  try{
     if (e.target.parentElement.className === "topnav") {
       x.className += " responsive";
     } else {
       x.className = "topnav";
     }
-  } catch (err) {
-    x.className = "topnav";
-  }
+  }catch(err) {x.className = "topnav"}
 }
-document.addEventListener("click", toggleNav);
+document.addEventListener('click',toggleNav);
 
-function navBar() {
-  const navbar = document.querySelector("#myTopnav");
-  navbar.innerHTML = `
+function navBar(){
+  const navbar = document.querySelector('#myTopnav');
+    navbar.innerHTML =`
       <a href="javascript:void(0);" style="font-size:15px;" class="icon">&#9776;</a>
       <div class="dropdown">
         <a href='#' class="dropbtn heh">Games</a>
@@ -87,189 +81,146 @@ function navBar() {
       <a href='op.html'>Orion Park</a>
       <a href='javascript:helpPage()'>Help</a>
     `;
-  document.querySelectorAll(".heh").forEach(function (item) {
-    return item.addEventListener("click", randomPage);
-  });
+  document.querySelectorAll('.heh').forEach(function(item) {return item.addEventListener('click',randomPage)});
 }
 
-function helpPage() {
+function helpPage(){
   let hashy = location.pathname.split(".")[0].split("/");
-  hashy = hashy[hashy.length - 1];
-  location = "help.html#" + hashy;
+  hashy=hashy[hashy.length-1];
+  location = 'help.html#'+hashy;
 }
 
 navBar();
 let test;
-const navs = ["Games", "Tools"];
-const randomChild = function (len) {
-  return Math.floor(Math.random() * len);
-};
+const navs = ['Games','Tools'];
+const randomChild = function(len) {return Math.floor(Math.random()*len)};
 function randomPage(e) {
   test = e;
-  if (
-    navs.some(function (nav) {
-      return e.target.innerHTML.includes(nav);
-    })
-  ) {
-    let nodeLen = randomChild(
-      e.target.parentElement.children[1].querySelectorAll("a").length
-    );
-    e.target.parentElement.children[1].querySelectorAll("a")[nodeLen].click();
-  } else return;
+  if (navs.some(function(nav) {return e.target.innerHTML.includes(nav)})){
+    let nodeLen = randomChild(e.target.parentElement.children[1].querySelectorAll('a').length)
+    e.target.parentElement.children[1].querySelectorAll('a')[nodeLen].click();
+  }
+  else return;
 }
 
 if ("serviceWorker" in navigator) {
   //Adds manifest and stuff
   let head = document.head;
-  let mani = document.createElement("link"),
-    apple = document.createElement("link"),
-    theme = document.createElement("meta");
-  mani.rel = "manifest";
-  apple.rel = "apple-touch-icon";
-  theme.name = "theme-color";
-  mani.href = "manifest.json";
-  apple.href = "images/apple-icon-180.png";
-  theme.content = "#800080";
-  head.appendChild(mani);
-  head.appendChild(apple);
-  head.appendChild(theme);
+  let mani = document.createElement("link"), apple = document.createElement("link"), theme = document.createElement("meta");
+  mani.rel = "manifest"; apple.rel = "apple-touch-icon"; theme.name = "theme-color";
+  mani.href = "manifest.json"; apple.href = "images/apple-icon-180.png"; theme.content = "#800080";
+  head.appendChild(mani); head.appendChild(apple); head.appendChild(theme);
 
   //Makes website available offline
-  window.addEventListener("load", function () {
+  window.addEventListener("load", function() {
     navigator.serviceWorker
       .register("sw.js")
-      .then((res) => console.log("service worker registered"))
-      .catch((err) => console.log("service worker not registered", err));
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
 
     let deferredPrompt;
     let body = document.querySelector("#main");
     let btn = document.createElement("button");
-    btn.classList.add("add-button", "button");
+    btn.classList.add("add-button","button");
     btn.innerText = "Add to home screen";
     body.appendChild(btn);
-    const addBtn = document.querySelector(".add-button");
-    addBtn.style.display = "none";
+    const addBtn = document.querySelector('.add-button');
+    addBtn.style.display = 'none';
 
     //Enable Progressive Web Apps on supported desktop browsers
-    window.addEventListener("beforeinstallprompt", (e) => {
+    window.addEventListener('beforeinstallprompt', (e) => {
       // Prevent Chrome 67 and earlier from automatically showing the prompt
       e.preventDefault();
       // Stash the event so it can be triggered later.
       deferredPrompt = e;
       // Update UI to notify the user they can add to home screen
-      addBtn.style.display = "block";
+      addBtn.style.display = 'block';
 
-      addBtn.addEventListener("click", (e) => {
-        // hide our user interface that shows our A2HS button
-        addBtn.style.display = "none";
-        // Show the prompt
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === "accepted") {
-            console.log("User accepted the A2HS prompt");
-          } else {
-            console.log("User dismissed the A2HS prompt");
-          }
-          deferredPrompt = null;
+      addBtn.addEventListener('click', (e) => {
+      // hide our user interface that shows our A2HS button
+      addBtn.style.display = 'none';
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
         });
       });
     });
-  });
+  })
 }
 
 //function to resize plot and copy to clipboard
-function clippy(x, y) {
-  let offset = document.querySelector("#graphdiv3").offsetTop;
-  document
-    .querySelector("#graphdiv3")
-    .setAttribute(
-      "style",
-      "height:" +
-        y +
-        "px !important; width:" +
-        x +
-        "px !important; margin: 0px 0px 10px 10px;"
-    );
-  window.dispatchEvent(new Event("resize"));
-  for (var j = 0; j < 3; j++) {
-    //weird way to make it actually work
+function clippy (x,y) {
+  let offset = document.querySelector('#graphdiv3').offsetTop;
+  document.querySelector('#graphdiv3').setAttribute('style', 'height:'+y+'px !important; width:'+x+'px !important; margin: 0px 0px 10px 10px;');
+  window.dispatchEvent(new Event('resize'));
+  for (var j = 0; j < 3; j++) {  //weird way to make it actually work
     html2canvas(document.querySelector("#graphdiv3"), {
       y: offset,
       //scrollY: -window.scrollY,
-      scrollX: 0,
-      scrollY: 0,
-      height: y + 30,
+      scrollX:0,
+      scrollY:0,
+      height: y+30,
       //width: x+30,
-    }).then((canvas) => {
-      if (typeof navigator.clipboard != "undefined") {
-        canvas.toBlob((blob) =>
-          navigator.clipboard.write([new ClipboardItem({ "image/png": blob })])
-        );
-      } else {
-        document.querySelector("#graphdiv3").innerHTML = "";
+    }).then(canvas => {
+      if (typeof(navigator.clipboard)!='undefined'){
+        canvas.toBlob(blob => navigator.clipboard.write([new ClipboardItem({'image/png': blob})]));
+      }
+      else{
+        document.querySelector("#graphdiv3").innerHTML='';
         document.querySelector("#graphdiv3").appendChild(canvas);
       }
     });
   }
-  if (typeof navigator.clipboard == "undefined") {
-    let htmltext =
-      navigator.userAgent.includes("Chrome") &&
-      !navigator.userAgent.includes("Edg")
-        ? "<br><br><a href=chrome://flags/#unsafely-treat-insecure-origin-as-secure>Auto copy to clipboard not supported in http. Copy this link and open in new tab to add this site as trusted to enable.</a>"
-        : "<br><br><a>Auto copy to clipboard not supported. Right click plot and copy as image.</a>";
-    let article = document.querySelector("article");
-    if (article.lastChild.nodeName != "A") article.innerHTML += htmltext;
+  if (typeof(navigator.clipboard)=='undefined') {
+    let htmltext = (navigator.userAgent.includes('Chrome') && !navigator.userAgent.includes("Edg")) ? "<br><br><a href=chrome://flags/#unsafely-treat-insecure-origin-as-secure>Auto copy to clipboard not supported in http. Copy this link and open in new tab to add this site as trusted to enable.</a>" : "<br><br><a>Auto copy to clipboard not supported. Right click plot and copy as image.</a>";
+    let article = document.querySelector('article');
+    if (article.lastChild.nodeName != "A") article.innerHTML+=htmltext;
   }
 }
 
 //Save the value function - save it to localStorage as (ID, VALUE)
-function saveValue(e) {
-  var id = e.id; // get the sender's id to save it .
+function saveValue(e){
+  var id = e.id;  // get the sender's id to save it .
   var val = e.value; // get the value.
-  localStorage.setItem(id, val); // Every time user writing something, the localStorage's value will override .
+  localStorage.setItem(id, val);// Every time user writing something, the localStorage's value will override .
 
-  let url = "";
+  let url ='';
   let params = {};
-  document.querySelectorAll("input").forEach((element) => {
+  document.querySelectorAll('input').forEach((element) => {
     if (element.value.length > 0) params[element.id] = element.value;
   });
   let esc = encodeURIComponent;
   let query = Object.keys(params)
-    .map((k) => esc(k) + "=" + esc(params[k]))
-    .join("&");
-  url += "?" + query;
+    .map(k => esc(k) + '=' + esc(params[k]))
+    .join('&');
+  url += '?' + query;
 
-  let newurl =
-    window.location.protocol +
-    "//" +
-    window.location.host +
-    window.location.pathname +
-    url;
-  window.history.pushState({ path: newurl }, "", newurl);
+  let newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + url;
+  window.history.pushState({ path: newurl }, '', newurl);
 }
 
 //Save the value function - save it to localStorage as (ID, VALUE)
-function saveRadio(e) {
+function saveRadio(e){
   e.checkbox = true;
-  document
-    .querySelectorAll('input[type="radio"]')
-    .forEach((rad) => localStorage.setItem(rad.id, rad.checked));
-  document
-    .querySelectorAll('input[type="checkbox"]')
-    .forEach((rad) => localStorage.setItem(rad.id, rad.checked));
+  document.querySelectorAll('input[type="radio"]').forEach(rad => localStorage.setItem(rad.id,rad.checked));
+  document.querySelectorAll('input[type="checkbox"]').forEach(rad => localStorage.setItem(rad.id,rad.checked));
 }
-function saveCheckbox(e) {
+function saveCheckbox(e){
   e.checkbox = true;
-  document
-    .querySelectorAll('input[type="checkbox"]')
-    .forEach((rad) => localStorage.setItem(rad.id, rad.checked));
+  document.querySelectorAll('input[type="checkbox"]').forEach(rad => localStorage.setItem(rad.id,rad.checked));
 }
 
 //get the saved value function - return the value of "v" from localStorage.
-function getSavedValue(v) {
+function getSavedValue  (v){
   if (!localStorage.getItem(v)) {
-    return ""; // You can change this to your defualt value.
+    return "";// You can change this to your defualt value.
   }
   return localStorage.getItem(v);
 }
@@ -280,36 +231,36 @@ function change(el) {
 
 function exportToCsv(filename, rows) {
   var processRow = function (row) {
-    var finalVal = "";
+    var finalVal = '';
     for (var j = 0; j < row.length; j++) {
-      var innerValue = row[j] === null ? "" : row[j].toString();
+      var innerValue = row[j] === null ? '' : row[j].toString();
       if (row[j] instanceof Date) {
         innerValue = row[j].toLocaleString();
-      }
+      };
       var result = innerValue.replace(/"/g, '""');
-      if (result.search(/("|,|\n)/g) >= 0) result = '"' + result + '"';
-      if (j > 0) finalVal += ",";
+      if (result.search(/("|,|\n)/g) >= 0)
+        result = '"' + result + '"';
+      if (j > 0)
+        finalVal += ',';
       finalVal += result;
     }
-    return finalVal + "\n";
+    return finalVal + '\n';
   };
-  var csvFile = "";
+  var csvFile = '';
   for (var i = 0; i < rows.length; i++) {
     csvFile += processRow(rows[i]);
   }
-  var blob = new Blob([csvFile], { type: "text/csv;charset=utf-8;" });
-  if (navigator.msSaveBlob) {
-    // IE 10+
+  var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+  if (navigator.msSaveBlob) { // IE 10+
     navigator.msSaveBlob(blob, filename);
   } else {
     var link = document.createElement("a");
-    if (link.download !== undefined) {
-      // feature detection
+    if (link.download !== undefined) { // feature detection
       // Browsers that support HTML5 download attribute
       var url = URL.createObjectURL(blob);
       link.setAttribute("href", url);
       link.setAttribute("download", filename);
-      link.style.visibility = "hidden";
+      link.style.visibility = 'hidden';
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
@@ -317,14 +268,14 @@ function exportToCsv(filename, rows) {
   }
 }
 
-window.post = function (url, data, sucPost) {
-  return fetch(url, { method: "POST", body: JSON.stringify(data) })
-    .then((response) => response.text())
-    .then(sucPost)
-    .catch((error) => {
-      console.log("Error:", data);
-    });
-};
+window.post = function(url, data, sucPost) {
+  return fetch(url, {method: "POST", body: JSON.stringify(data)})
+  .then(response => response.text())
+  .then(sucPost)
+  .catch((error) => {
+    console.log('Error:', data);
+  });
+}
 
 //fade in and fadeout
 function _(el) {
@@ -335,15 +286,15 @@ function _(el) {
 }
 
 _.prototype.fade = function fade(type, ms) {
-  var isIn = type === "in",
+  var isIn = type === 'in',
     opacity = isIn ? 0 : 1,
     interval = 50,
     duration = ms,
     gap = interval / duration,
     self = this;
 
-  if (isIn) {
-    self.el.style.display = "inline";
+  if(isIn) {
+    self.el.style.display = 'inline';
     self.el.style.opacity = opacity;
   }
 
@@ -351,10 +302,10 @@ _.prototype.fade = function fade(type, ms) {
     opacity = isIn ? opacity + gap : opacity - gap;
     self.el.style.opacity = opacity;
 
-    if (opacity <= 0) self.el.style.display = "none";
-    if (opacity <= 0 || opacity >= 1) window.clearInterval(fading);
+    if(opacity <= 0) self.el.style.display = 'none'
+    if(opacity <= 0 || opacity >= 1) window.clearInterval(fading);
   }
   var fading = window.setInterval(func, interval);
-};
+}
 
-document.documentElement.setAttribute("lang", navigator.language); //add language to html
+document.documentElement.setAttribute('lang', navigator.language); //add language to html
--- js/memory.js
+++ js/memory.js
@@ -1,112 +1,92 @@
-const scoreBoard = document.querySelector(".score");
+const scoreBoard = document.querySelector('.score');
 const mode = document.querySelector("#Mode");
-const board = document.querySelector(".memory");
-const radius = board.offsetHeight / 2;
-const toDeg = (rad) => (rad * 180) / Math.PI;
-const Deg = (x, y) => toDeg(Math.atan(y / x));
-let colArr = [],
-  clickArr = [],
-  colClick;
+const board = document.querySelector('.memory');
+const radius = board.offsetHeight/2;
+const toDeg = (rad) =>  rad * 180/Math.PI;
+const Deg = (x,y) => toDeg(Math.atan(y/x));
+let colArr = [], clickArr = [], colClick;
 let gamesStarted = false;
 let score = 0;
 let time = 0;
 let oldnum, oldernum;
 
-const classes = ["red", "green", "blue", "orange"];
+const classes = ['red','green','blue','orange'];
 
-function color(x, y) {
-  const deg = Deg(x, y);
+function color(x,y) {
+  const deg = Deg(x,y);
   let col;
-  if (deg < 45 && deg > -45 && x > 0) (col = "green"), k(10, 150, 200);
-  if (Math.abs(deg) > 45 && y > 0) (col = "blue"), k(10, 200, 200);
-  if (deg < 45 && deg > -45 && x < 0) (col = "orange"), k(10, 100, 200);
-  if (Math.abs(deg) > 45 && y < 0) (col = "red"), k(10, 50, 200);
+  if (deg < 45 && deg > -45 && x > 0) col = 'green', k(10,150,200);
+  if (Math.abs(deg) > 45 && y > 0) col = 'blue', k(10,200,200);
+  if (deg < 45 && deg > -45 && x < 0) col = 'orange', k(10,100,200);
+  if (Math.abs(deg) > 45 && y < 0) col = 'red', k(10,50,200);
   clickArr.push(col);
   return col;
 }
 
 function clicked(e) {
-  if (!e.isTrusted) return; // cheater!
+  if(!e.isTrusted) return; // cheater!
   colClick = color(e.offsetX, e.offsetY);
   board.classList.add(colClick);
-  for (let i = 0; i < clickArr.length; i++) {
+  for(let i=0;i<clickArr.length;i++){
     if (clickArr[i] != colArr[i]) return gameOver();
   }
   setTimeout(() => {
     board.classList.remove(colClick);
   }, 200);
 
-  if (
-    JSON.stringify(clickArr) == JSON.stringify(colArr) &&
-    clickArr.length == colArr.length
-  ) {
+  if (JSON.stringify(clickArr) == JSON.stringify(colArr) && clickArr.length == colArr.length) {
     setTimeout(() => {
-      score++, (scoreBoard.textContent = score);
+      score++,
+      scoreBoard.textContent = score;
       repeatArr();
     }, 200);
   }
 }
 
-function gameOver() {
+function gameOver(){
   board.classList.add(...classes);
-  setTimeout(() => {
-    k(10, 40, 300);
-    board.classList.remove("blue", "red");
-  }, 300);
-  setTimeout(() => {
-    k(10, 30, 300);
-    board.classList.add("blue", "red");
-    board.classList.remove("green", "orange");
-  }, 600);
-  setTimeout(() => {
-    k(10, 20, 300);
-    board.classList.remove(...classes);
-  }, 900);
-  board.removeEventListener("click", clicked);
-  (colArr = []), (clickArr = []);
+  setTimeout(() => {k(10,40,300);board.classList.remove('blue', 'red');}, 300);
+  setTimeout(() => {k(10,30,300);board.classList.add('blue', 'red');board.classList.remove('green', 'orange');}, 600);
+  setTimeout(() => {k(10,20,300);board.classList.remove(...classes);}, 900);
+  board.removeEventListener('click', clicked);
+  colArr = [], clickArr = [];
   gamesStarted = false;
-  board.classList.add("waiting");
+  board.classList.add('waiting');
   saveScores(score);
 }
 
-function repeatArr() {
-  board.removeEventListener("click", clicked);
-  let freq,
-    ind = 0;
+function repeatArr(){
+  board.removeEventListener('click', clicked);
+  let freq, ind = 0;
 
   (function myLoop(i) {
-    setTimeout(function () {
+    setTimeout(function() {
       let cl = colArr[ind++];
       board.classList.add(cl);
-      if (cl == "red") freq = 50;
-      if (cl == "green") freq = 150;
-      if (cl == "blue") freq = 200;
-      if (cl == "orange") freq = 100;
-      k(10, freq, 200);
+      if (cl == 'red') freq = 50;
+      if (cl == 'green') freq = 150;
+      if (cl == 'blue') freq = 200;
+      if (cl == 'orange') freq = 100;
+      k(10,freq,200);
       setTimeout(() => {
         board.classList.remove(...classes);
       }, 800);
-      if (--i) myLoop(i);
-      //  decrement i and call myLoop again if i > 0
-      else
-        setTimeout(() => {
-          nextColor();
-        }, 1000);
-    }, 1000);
-  })(colArr.length); //  pass the number of iterations as an argument
+      if (--i) myLoop(i);   //  decrement i and call myLoop again if i > 0
+      else setTimeout(() => {nextColor();}, 1000);
+    }, 1000)
+  })(colArr.length);  //  pass the number of iterations as an argument
 }
 
 function randomColor() {
   let num = Math.floor(Math.random() * 4);
-  if (num == oldernum) (oldernum = null), randomColor();
-  //only two colors in a row
+  if (num == oldernum) oldernum=null,randomColor(); //only two colors in a row
   else innerRandom();
-  function innerRandom() {
+  function innerRandom(){
     let colo;
-    if (num == 0) (colo = "red"), k(10, 50, 200);
-    if (num == 1) (colo = "green"), k(10, 150, 200);
-    if (num == 2) (colo = "blue"), k(10, 200, 200);
-    if (num == 3) (colo = "orange"), k(10, 100, 200);
+    if (num == 0) colo = 'red', k(10,50,200);
+    if (num == 1) colo = 'green', k(10,150,200);
+    if (num == 2) colo = 'blue', k(10,200,200);
+    if (num == 3) colo = 'orange', k(10,100,200);
     colArr.push(colo);
     board.classList.add(colo);
     oldernum = oldnum;
@@ -115,44 +95,37 @@ function randomColor() {
 }
 
 function nextColor() {
-  board.removeEventListener("click", clicked);
+  board.removeEventListener('click', clicked);
   mode.textContent = "Easy";
+  setTimeout(() => {randomColor();}, 200);
+  setTimeout(() => {board.classList.remove(...classes);}, 1000);
   setTimeout(() => {
-    randomColor();
-  }, 200);
-  setTimeout(() => {
-    board.classList.remove(...classes);
-  }, 1000);
-  setTimeout(() => {
-    board.addEventListener("click", clicked), (clickArr = []);
+    board.addEventListener('click', clicked), clickArr = [];
   }, 1200);
 }
 
 function startGame() {
   if (gamesStarted) return;
-  board.classList.remove("waiting");
+  board.classList.remove('waiting');
   gamesStarted = true;
-  (colArr = []), (clickArr = []);
+  colArr = [], clickArr = [];
   scoreBoard.textContent = 0;
   score = 0;
   nextColor();
 }
 
-var AudioContext =
-  window.AudioContext || // Default
-  window.webkitAudioContext || // Safari and old versions of Chrome
-  false;
-if (AudioContext) {
-  var a = new AudioContext();
-}
+var AudioContext = window.AudioContext // Default
+    || window.webkitAudioContext // Safari and old versions of Chrome
+    || false;
+if (AudioContext) {var a = new AudioContext();}
 
 try {
-  getScores("uploads/simonscores.json", "simonScores");
-} catch (err) {}
+  getScores('uploads/simonscores.json',"simonScores");
+}catch(err){}
 
-board.classList.add("waiting");
+board.classList.add('waiting');
 
 //startup
-document.cookie = "game=simon";
+document.cookie="game=simon";
 document.getElementById("userName").value = getSavedValue("userName");
 ip();
