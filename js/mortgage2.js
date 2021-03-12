8cfb8b6b24f948ea8061afe4a38f9ba79de3b97e
HansEJC <66210680+HansEJC@users.noreply.github.com>
2021-03-12 17:18:52 +0000

    Update mortgage

--- js/mortgage2.js
+++ js/mortgage2.js
@@ -1,52 +1,72 @@
 function firstMortgage(info){
-  const sd = new Date(2017,4,1); //start of mortgage
-  const iir = 4.09/12/100; //initial interest rate
+  const ir = 4.09/12/100; //initial interest rate
   let {teq,mortgage,rl,tpa} = info;
   for (let i = 0; i < 13; i++) { //first year
-    let intr = rl*iir; //interest
+    let intr = rl*ir; //interest
     let eqp = 1000-intr; //equity payment
-    teq = teq + eqp;
-    mortgage.push([new Date(2017,sd.getMonth()+i,sd.getDay()), intr, eqp, intr+eqp]);
+    teq += eqp;
+    mortgage.push([new Date(2017,3+i,1), intr, eqp, intr+eqp]); //start of mortgage
     rl = rl-eqp;
-    tpa = tpa +1000;
+    tpa +=1000;
   }
   for (let i = 0; i < 11; i++) { // changed overpayment to £1100
-    let intr = rl*iir;
+    let intr = rl*ir;
     let eqp = 1100-intr;
-    teq = teq + eqp;
-    mortgage.push([new Date(2018,5+i,1), intr, eqp, intr+eqp]);
+    teq += eqp;
+    mortgage.push([new Date(2018,4+i,1), intr, eqp, intr+eqp]);
     rl = rl-eqp;
-    tpa = tpa +1100;
+    tpa +=1100;
   }
   return {teq,mortgage,rl,tpa};
 }
 
 function secondMortgage(info){
   let today = new Date();
-  const monthd = (today-new Date(2020,7,1))/1000/60/60/24/365.25*12; //difference in month
-  const nir = 1.84/12/100; //remortgage rate
+  const ir = 1.84/12/100; //remortgage rate
   let {teq,mortgage,rl,tpa} = info;
-  let intr = rl*nir;
+  let intr = rl*ir;
   let eqp = 1100-intr-995; //the 995 was te remortgage fee
-  teq = teq + eqp;
-  mortgage.push([new Date(2019,4,1), intr, eqp, intr+eqp]);
+  teq += eqp;
+  mortgage.push([new Date(2019,3,1), intr, eqp, intr+eqp]);
   rl = rl-eqp;
-  tpa = tpa + 1100;
+  tpa += 1100;
   for (let i = 0; i < 14; i++) { // changed overpayment to £1200
-    intr = rl*nir;
+    intr = rl*ir;
     eqp = 1200-intr;
-    teq = teq + eqp;
-    mortgage.push([new Date(2019,5+i,1), intr, eqp, intr+eqp]);
+    teq += eqp;
+    mortgage.push([new Date(2019,4+i,1), intr, eqp, intr+eqp]);
     rl = rl-eqp;
-    tpa = tpa +1200;
+    tpa +=1200;
   }
-  for (let i = 0; i < monthd; i++) { // changed overpayment to £1500
-    intr = rl*nir;
+  for (let i = 0; i < 9; i++) { // changed overpayment to £1500
+    intr = rl*ir;
     eqp = 1500-intr;
-    teq = teq + eqp;
-    mortgage.push([new Date(2020,7+i,1), intr, eqp, intr+eqp]);
+    teq += eqp;
+    mortgage.push([new Date(2020,6+i,1), intr, eqp, intr+eqp]);
     rl = rl-eqp;
-    tpa = tpa +1500;
+    tpa +=1500;
+  }
+  return {teq,mortgage,rl,tpa};
+}
+
+function currentMortgage(info){
+  let today = new Date();
+  const monthd = (today-new Date(2021,3,1))/1000/60/60/24/365.25*12; //difference in month
+  const ir = 1.59/12/100; //remortgage rate
+  let {teq,mortgage,rl,tpa} = info;
+  let intr = rl*ir;
+  let eqp = 459.89-intr+5814.12+250-66; //the 5748 was the shortfall since we borrowed £135k, the £250 is cashback, £66 fees
+  teq += eqp;
+  mortgage.push([new Date(2021,3,1), intr, eqp, intr+eqp]);
+  rl = rl-eqp;
+  tpa += 459.89;
+  for (let i = 0; i < monthd; i++) { // changed overpayment to £1500
+    intr = rl*ir;
+    eqp = 1500-intr;
+    teq += eqp;
+    mortgage.push([new Date(2021,4+i,1), intr, eqp, intr+eqp]);
+    rl = rl-eqp;
+    tpa +=1500;
   }
   return {teq,mortgage,rl,tpa,monthd};
 }
@@ -54,24 +74,24 @@ function secondMortgage(info){
 function futureMortgage(info){
   let {intr,eqp,mortgage,rl,tp} = info;
   let pn = 0; //payment number
-  const nir = 1.84/12/100; //remortgage rate
+  const ir = 1.59/12/100; //remortgage rate
   var start = 0; var max = 10000;
   for (let i = 0; rl > 0; i++) {
-    intr = rl*nir;
+    intr = rl*ir;
     eqp = tp-intr;
-    mortgage.push([new Date(2020,8+i+monthd,1), intr, eqp, intr+eqp]);
+    mortgage.push([new Date(2021,5+i+monthd,1), intr, eqp, intr+eqp]);
     rl = rl-eqp;
     pn++;
     start+=1;if(start>max)break;
   }
-  return {intr,eqp,mortgage,monthd,tp,pn};
+  return pn;
 }
 
 function calculations(){
   const il = 176225; //initial loan
   let mortgage = [];
   let rl, teq, ltv, tpa; //remaining loan, total equity, total payed
-  const mpc = 555.07;  //current mortgage payment
+  const mpc = 459.89;  //current mortgage payment
   var op = Number(getSavedValue("OP"));
   var tp = op+mpc; // total payment
 
@@ -84,8 +104,9 @@ function calculations(){
 
   let info = {teq,mortgage,rl,tpa,tp};
   info = firstMortgage(info);
-  info = {monthd,tpa,rl,teq} = secondMortgage(info);  
-  ({pn} = futureMortgage({tp,...info}));
+  info = secondMortgage(info);
+  info = {monthd,tpa,rl,teq} = currentMortgage(info);  
+  let pn = futureMortgage({tp,...info});
 
   ltv = 100 - teq / (teq + rl) * 100;
 
@@ -112,6 +133,7 @@ function dygPlot(mortgage){
     document.getElementById("graphdiv3"),
     mortgage,
     {
+      valueRange : [0,2000], //sets default view to £2k
       legend: 'always',
       labels: [ 'Date', 'Interest','Equity', 'Payment'],
       xlabel: "Dates",
