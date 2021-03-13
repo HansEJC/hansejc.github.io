e8efbe9e75900b8d0ee5095e1ae8b4d0f58c9b6f
HansEJC <66210680+HansEJC@users.noreply.github.com>
2021-03-13 18:16:53 +0000

    Deepscan improvements

--- js/mortgage2.js
+++ js/mortgage2.js
@@ -11,27 +11,33 @@ function mortgageLoop(loopInfo) {
   return {teq,mortgage,rl,tpa};
 }
 
-function firstMortgage(info){
+function firstMortgage(){
+  const il = 176225; //initial loan
+  let rl, teq, ltv, tpa; //remaining loan, total equity, total payed
+
+  document.getElementById("HPI").value = getSavedValue("HPI");    // set the value to this input
+  document.getElementById("OP").value = getSavedValue("OP");
+  let hpi = Math.max(il + 9275, +(getSavedValue("HPI")));    // set the value to this input
+  teq = hpi - il; //initial deposit plus HPI
+  rl = il; tpa = 0;
+  
+  let mortgage = [];
   const ir = 4.09/12/100; //initial interest rate
-  let {teq,mortgage,rl,tpa} = info;
-  let loopInfo = {num: 13, pay: 1000, year: 2017, month: 3,ir,...info};
-  info = {teq,mortgage,rl,tpa} = mortgageLoop(loopInfo);  
+  let loopInfo = {num: 13, pay: 1000, year: 2017, month: 3,ir,teq,rl,tpa,mortgage};
+  let info = mortgageLoop(loopInfo);  
   
   loopInfo = {num: 11, pay: 1100, year: 2018, month: 4,ir,...info}; // changed overpayment to £1100
   return mortgageLoop(loopInfo);
 }
 
 function secondMortgage(info){
-  let today = new Date();
-  const ir = 1.84/12/100; //remortgage rate
-  let {teq,mortgage,rl,tpa} = info;
-  
+  const ir = 1.84/12/100; //remortgage rate  
   let pay = 1100-995; //the 995 was te remortgage fee
   let loopInfo = {num: 1, pay, year: 2019, month: 3,ir,...info}; // First month
-  info = {teq,mortgage,rl,tpa} = mortgageLoop(loopInfo);
+  info = mortgageLoop(loopInfo);
 
   loopInfo = {num: 14, pay: 1200, year: 2019, month: 4,ir,...info}; // changed overpayment to £1200
-  info = {teq,mortgage,rl,tpa} = mortgageLoop(loopInfo);
+  info = mortgageLoop(loopInfo);
 
   loopInfo = {num: 9, pay: 1500, year: 2020, month: 6,ir,...info}; // changed overpayment to £1500
   return mortgageLoop(loopInfo);
@@ -41,23 +47,22 @@ function currentMortgage(info){
   let today = new Date();
   const monthd = (today-new Date(2021,3,1))/1000/60/60/24/365.25*12; //difference in month
   const ir = 1.59/12/100; //remortgage rate
-  let {teq,mortgage,rl,tpa} = info;
 
   let pay = 459.89+5814.12+250-66; //the £5814.12 was the shortfall since we borrowed £135k, the £250 is cashback, £66 fees
   let loopInfo = {num: 1, pay, year: 2021, month: 3,ir,...info}; // First month
-  info = {teq,mortgage,rl,tpa} = mortgageLoop(loopInfo);
+  info = mortgageLoop(loopInfo);
   
   loopInfo = {num: monthd, pay: 1500, year: 2021, month: 4,ir,...info}; // changed overpayment to £1500
   return {monthd,...mortgageLoop(loopInfo)};
 }
 
 function futureMortgage(info){
-  let {intr,eqp,mortgage,rl,tp} = info;
+  let {mortgage,rl,tp,monthd} = info;
   let pn = 0; //payment number
   const ir = 1.59/12/100; //remortgage rate
   for (let i = 0; rl > 0; i++) {
-    intr = rl*ir;
-    eqp = tp-intr;
+    let intr = rl*ir;
+    let eqp = tp-intr;
     mortgage.push([new Date(2021,5+i+monthd,1), intr, eqp, intr+eqp]);
     rl = rl-eqp;
     pn++;
@@ -66,39 +71,30 @@ function futureMortgage(info){
 }
 
 function calculations(){
-  const il = 176225; //initial loan
-  let mortgage = [];
-  let rl, teq, ltv, tpa; //remaining loan, total equity, total payed
+
   const mpc = 459.89;  //current mortgage payment
   var op = +(getSavedValue("OP"));
   var tp = op+mpc; // total payment
-
-  document.getElementById("HPI").value = getSavedValue("HPI");    // set the value to this input
-  document.getElementById("OP").value = getSavedValue("OP");
-  let hpi = Math.max(il + 9275, +(getSavedValue("HPI")));    // set the value to this input
-  teq = hpi - il; //initial deposit plus HPI
-  rl = il; tpa = 0;
-
-  let info = {teq,mortgage,rl,tpa,tp};
-  info = firstMortgage(info);
+  
+  let info = firstMortgage();
   info = secondMortgage(info);
-  info = {monthd,tpa,rl,teq} = currentMortgage(info);  
+  info = {teq,mortgage,rl,tpa} = currentMortgage(info); 
   let pn = futureMortgage({tp,...info});
 
   ltv = 100 - teq / (teq + rl) * 100;
 
-  spanToMoney("TPA",tpa);
-  spanToMoney("TEQ",teq);
-  spanToMoney("MPN",mpc);
-  spanToMoney("TP",tp);
-  spanToMoney("RD",rl);
-  spanToMoney("MOP",rl/120);
+  spanMon("TPA",tpa);
+  spanMon("TEQ",teq);
+  spanMon("MPN",mpc);
+  spanMon("TP",tp);
+  spanMon("RD",rl);
+  spanMon("MOP",rl/120);
   document.getElementById("LTV").textContent = ltv.toFixed(0)+" %";
 
   if ((rl/120)<op) document.getElementById("MOP").className = 'label danger';
   else document.getElementById("MOP").className = 'label safe';
 
-  document.getElementById("RT").textContent = Math.floor((pn)/12)+" y " + (12*((pn)/12-Math.floor(pn/12))).toFixed(0)+" m";
+  document.getElementById("RT").textContent = `${Math.floor((pn)/12)} y ${(12*((pn)/12-Math.floor(pn/12))).toFixed(0)} m`;
   dygPlot(mortgage);
 }
 
@@ -120,7 +116,7 @@ function dygPlot(mortgage){
       axes: {
         y: {
           axisLabelFormatter: function(y) {
-            return  y + ' £';
+            return  `${y} £`;
           },
           axisLabelWidth: 60
         }
@@ -132,7 +128,7 @@ function dygPlot(mortgage){
   }, 500);
 }
 
-const spanToMoney = (span,cont) => document.getElementById(span).textContent = (cont).toFixed(2).toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")+" £";
+const spanMon = (span,cont) => document.getElementById(span).textContent = `${(cont).toFixed(2).toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}  £`;
 
 //startup
 calculations();
