3cbf9750b09a9c6c14c25bacad4c2d00ad87bdc9
HansEJC <66210680+HansEJC@users.noreply.github.com>
2021-03-29 14:04:37 +0100

    OP Mod
    Will use the txt file instead of downloading the xlsx file.

--- Orion Park/copydel.bat
+++ Orion Park/copydel.bat
@@ -6,9 +6,6 @@ for /F "delims=" %%I in ('dir "\\192.168.1.14\PSP_Orionpark\Workshop Stock & Del
 :FileCopied
 rem More commands after copying file with newest last modification date.
 
-::copy "\\192.168.1.14\PSP_Orionpark\Workshop Stock & Delivery Records\Y01-AA-FOR-0098 A01 Workshop Stock Record and Y01-AA-FOR-0099-Workshop Delivery Record - UPDATED1.xlsx" op.xlsx
-
-::.\XlsToCsv.vbs delivery.xlsx delivery.csv
 .\ExcelConverter.js
-::PAUSE
 
+powershell -Command "& {(Get-Item 'C:\xampp\htdocs\dashboard\Orion Park\op.xlsx').LastWriteTime > mod.txt;}"
