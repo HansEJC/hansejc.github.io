bb7e5eceff787b8672f567a4c5815f3554f8fa03
HansEJC <66210680+HansEJC@users.noreply.github.com>
2021-06-03 21:37:55 +0100

    Deepsource fixes

--- js/scores.js
+++ js/scores.js
@@ -1,15 +1,15 @@
 fireBase();
 function saveScores(scr) {
-  let game = getSavedValue("game");
+  const game = getSavedValue("game");
   let name = document.querySelector("#userName").value;
   name = name === `` ? ` ` : name;
-  let scores = JSON.parse(localStorage.getItem(game));
-  let date = new Date().toLocaleString("en-GB", { timeZone: "Europe/London" });
+  const scores = JSON.parse(localStorage.getItem(game));
+  const date = new Date().toLocaleString("en-GB", { timeZone: "Europe/London" });
   let exists = false;
   try {
-    if (scores[name] != undefined) {
-      let score = scores[name].score;
-      let oldDate = scores[name].date;
+    if (typeof scores[name] !== `undefined`) {
+      const { score } = scores[name];
+      const oldDate = scores[name].date;
       scores[name].date = scr > score ? date : oldDate;
       scores[name].lastdate = date;
       scores[name].score = Math.max(score, scr);
@@ -21,7 +21,7 @@ function saveScores(scr) {
   if (!exists) {
     scores[name] = {
       score: scr,
-      date: date,
+      date,
       lastdate: date,
     }
   }
@@ -33,16 +33,16 @@ function saveScores(scr) {
 function sucPost(data) {
   document.getElementById("TempScore").innerHTML = data;
   _('#TempScore').fade('in', 100);
-  setTimeout(function () {
+  setTimeout(() => {
     _('#TempScore').fade('out', 500);
   }, 1000);
 }
 
 function getScores(full) {
-  let game = getSavedValue("game");
-  let dbObj = firebase.database().ref(game);
+  const game = getSavedValue("game");
+  const dbObj = firebase.database().ref(game);
   dbObj.on(`value`, snap => {
-    let scores = snap.val();
+    const scores = snap.val();
     localStorage.setItem(game, JSON.stringify(scores));
     scoreTable(scores, full);
   });
@@ -56,8 +56,8 @@ function scoreTable(scores, full) {
   scores = Object.entries(scores);
   scores.sort((a, b) => b[1].score - a[1].score);
   scores.forEach(val => {
-    let name = val[0].substr(0, 10);
-    let row = table.insertRow(-1);
+    const name = val[0].substr(0, 10);
+    const row = table.insertRow(-1);
     row.insertCell(0).innerHTML = name;
     row.insertCell(1).innerHTML = val[1].score;
     if (full) {
@@ -66,15 +66,15 @@ function scoreTable(scores, full) {
     }
   });
   if (full) {
-    var imported = document.createElement('script');
+    const imported = document.createElement('script');
     imported.src = 'js/ext/sorttable.js';
     document.head.appendChild(imported);
   }
 }
 
 function sendData(name, data) {
-  let game = getSavedValue("game");
-  let dbObj = firebase.database().ref(`${game}/${name}`);
+  const game = getSavedValue("game");
+  const dbObj = firebase.database().ref(`${game}/${name}`);
   dbObj.update(data);
   try {
     dataLayer.push({
@@ -89,8 +89,8 @@ function sendData(name, data) {
 // gain, frequency, duration
 function k(w, x, y) {
   try {
-    let v = a.createOscillator();
-    let u = a.createGain();
+    const v = a.createOscillator();
+    const u = a.createGain();
     v.connect(u);
     v.frequency.value = x;
     v.type = "square";
